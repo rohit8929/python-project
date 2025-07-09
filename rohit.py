6cb860f9b07ee62a7318dfbe329bf9517a21f2e9
Rohit <rohit.verma.8929@gmail.com>
2025-07-09 12:31:36 +0530

    Add files via upload

---
+++ rohit.py
@@ -0,0 +1,11 @@
+
+num1 = float(input("Enter the first number"))
+num2 = float(input("Enter the second number"))
+sum = num1 + num2
+multiply = num1 * num2
+divide = num1 / num2
+Subtraction = num1 - num2
+print("Addition", sum)
+print("Multiply", multiply)
+print("Division", divide)
+print("Subtraction", Subtraction)
